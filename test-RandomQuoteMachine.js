1e433dcf8dfab8e43f773cf93d88a4cf44bc2a72
Maja <manon.jacquin@gmail.com>
2017-11-18 14:26:52 -0200

    creates new test

--- test-RandomQuoteMachine.js
+++ test-RandomQuoteMachine.js
@@ -1,10 +1,6 @@
 import test from 'ava';
-import Random_Quote_Machine from "../../index";
+import Random_Quote_Machine from "../../assets/index";
 
-test('quote', t => {
-	t.is(newQuoteGenerated);
-});
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
+test('random quote is generated', t => {
+	t.is(index.getRandomQuote(quotes), quotes[Math.floor((Math.random() * (quotes.length)))]);
 });
